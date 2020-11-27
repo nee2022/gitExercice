227b9714546643dee68532e5097b9fedd5305084
zhangyihua <zhangyihua_2020@qq.com>
2020-11-27 11:34:49 +0800

    aside添加element-ui初始化

--- src/pages/merchantAssets/merchantAssetsIndex.vue
+++ src/pages/merchantAssets/merchantAssetsIndex.vue
@@ -1,7 +1,52 @@
 <template>
   <div class="wrapper">
     <aside>
-      123
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>商户账号</span>
+          </template>
+          <el-menu-item index="1-1">所有</el-menu-item>
+          <el-menu-item index="1-2">分润账号</el-menu-item>
+          <el-menu-item index="1-3">自营账号</el-menu-item>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>财务管理</span>
+          </template>
+          <el-menu-item index="1-1">订单记录</el-menu-item>
+          <el-menu-item index="1-2">分成记录</el-menu-item>
+          <el-menu-item index="1-3">提现记录</el-menu-item>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>自营网关</span>
+          </template>
+          <el-menu-item index="1-1">支付网关</el-menu-item>
+          <el-menu-item index="1-2">发票网关</el-menu-item>
+          <el-menu-item index="1-3">短信网关</el-menu-item>
+        </el-submenu>
+        <el-submenu index="4">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>开放平台</span>
+          </template>
+        </el-submenu>
+        <el-submenu index="5">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>操作日志</span>
+          </template>
+        </el-submenu>
+      </el-menu>
     </aside>
     <section>
       123
@@ -10,7 +55,16 @@
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
@@ -25,6 +79,7 @@ aside
   // border 1px solid black
   width px(280)
   height 100vh
+  background #1E69FE
 
 section
   float right
